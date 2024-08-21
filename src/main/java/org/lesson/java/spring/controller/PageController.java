444ffb8cb7f43b299516df18985c604ef55dcc06
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-08-21 19:56:35 +0200

    fix: methods changed to simplified version

--- src/main/java/org/lesson/java/spring/controller/PageController.java
+++ src/main/java/org/lesson/java/spring/controller/PageController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -27,63 +28,87 @@ public class PageController {
 	  @GetMapping("/movies")
 	  public String movies(Model model) throws FileNotFoundException {
 		  
-		  model.addAttribute("name", name);
-		  model.addAttribute("movieList", getBestMovies());
+		  String bestMovies = "";
 		  
-		return "movies";
+		  for(Movie movie : getBestMovies()) {
+			  
+			  bestMovies += movie.getMovieTitle() + ", ";		  
+			  
+		  }
+		  
+		  model.addAttribute("name", name);
+		  model.addAttribute("title", "List of the BEST movies");
+		  model.addAttribute("list", bestMovies);
+		  
+		  return "list";
 		  
 	  }
 	  
 	  @GetMapping("/songs")
 	  public String songs(Model model) throws FileNotFoundException {
+		 
+		  String bestSongs = "";
+		  
+		  for(Song song : getBestSongs()) {
+			  
+			  bestSongs += song.getSongTitle() + ", ";	
+			  
+		  }
 		  
 		  model.addAttribute("name", name);
-		  model.addAttribute("songList", getBestSongs());
+		  model.addAttribute("title", "List of the BEST songs");
+		  model.addAttribute("list", bestSongs);
 		  
-		return "songs";
 		  
+		  return "list";
+ 
 	  }
 
 	  
 	  
+
 	  
-	  //Method that return a string with the titles of movie objects
-	  
-	  private String getBestMovies() throws FileNotFoundException {
+	  private List<Movie> getBestMovies(){
 		  
-		  String bestMovies = "";
+		  
 		  
 		  //List used to acquire the movie objects 
-		  ArrayList<Movie> bestMoviesList = BestOfListManager.generateBestMovies();
-		  		  
-		  //Cycle used to concatenate the movie titles into one string
-		  for(int i = 0; i < bestMoviesList.size(); i++ ) {
-			  
-			  bestMovies = bestMovies + bestMoviesList.get(i).getMovieTitle();
-			  
-			  if(i < bestMoviesList.size() - 1 )
-				  bestMovies = bestMovies + ", ";
-		  }
+		  List<Movie> bestMovies = new ArrayList<Movie>();
+		  bestMovies.add(new Movie(0,"There will be blood"));
+		  bestMovies.add(new Movie(1,"Fight club"));
+		  bestMovies.add(new Movie(2,"Kill Bill"));
+		  bestMovies.add(new Movie(3,"The big Lebwoski"));
+		  bestMovies.add(new Movie(4,"Lo chiamavano Jeeg Robot"));
+		  bestMovies.add(new Movie(5,"The world's end"));
+		  bestMovies.add(new Movie(6,"The thing"));
+		  bestMovies.add(new Movie(7,"The lord of the rings"));
+		  bestMovies.add(new Movie(8,"Goodfellas"));
+		  bestMovies.add(new Movie(9,"Memories of murder"));
+		
+		  
 		  
 		  return bestMovies;
 		  
 	  }
 	  
-	//Method that return a string with the titles of song objects, analog to getBestMovie method
-	  
-	  private String getBestSongs() throws FileNotFoundException {
-		  
-		  String bestSongs = "";
-		  ArrayList<Song> bestSongsList = BestOfListManager.generateBestSongs();
+	  private List<Song> getBestSongs(){
 		  
 		  
-		  for(int i = 0; i < bestSongsList.size(); i++ ) {
-			  
-			  bestSongs = bestSongs + bestSongsList.get(i).getSongTitle();
-			  
-			  if(i < bestSongsList.size() - 1 )
-				  bestSongs = bestSongs + ", ";
-		  }
+		  
+		  //List used to acquire the movie objects 
+		  List<Song> bestSongs = new ArrayList<Song>();
+		  bestSongs.add(new Song(0,"Moonchild"));
+		  bestSongs.add(new Song(1,"The Rover"));
+		  bestSongs.add(new Song(2,"Escape from midwich valley"));
+		  bestSongs.add(new Song(3,"The price"));
+		  bestSongs.add(new Song(4,"War"));
+		  bestSongs.add(new Song(5,"Bruco Gianluco"));
+		  bestSongs.add(new Song(6,"Post(?) Organic"));
+		  bestSongs.add(new Song(7,"Memento mori"));
+		  bestSongs.add(new Song(8,"The old Haunt"));
+		  bestSongs.add(new Song(9,"Tout petit moineau"));
+		
+		  
 		  
 		  return bestSongs;
 		  
